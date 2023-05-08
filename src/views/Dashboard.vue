eb9da89dabf80b227bdcdcb545916bf77d0b34eb
mukhriddin-dev <khodievm@gmail.com>
2023-05-08 16:55:33 +0500

    admin panel success

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,7 +7,7 @@ import Sidebar from "../components/Sidebar/Sidebar.vue";
     <section id="main" class="w-full">
       <Sidebar />
       <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+        <div class="border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
      
          <router-view></router-view>
      
