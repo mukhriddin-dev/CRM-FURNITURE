493cfa27293d5fb4bc313f051996cc35b70a178d
mukhriddin-dev <khodievm@gmail.com>
2023-05-08 16:02:29 +0500

    wasdfg

---
+++ src/constants/navlinks.js
@@ -0,0 +1,48 @@
+export const navLinks = [
+    {
+        id: 1,
+        title: "Dashboard",
+        link: "/",
+        icon: "bx bxs-dashboard"
+    },
+    {
+        id: 2,
+        title: "Xodimlar",
+        link: "/employee",
+        icon: "bx bxs-user-plus"
+    },
+
+    {
+        id: 8,
+        title: 'Mahsulotlar',
+        link: "/product",
+        icon: "bx bxs-basket"
+    },
+    {
+        id: 4,
+        title: 'Buyurtmalar',
+        link: "/order",
+        icon: "bx bxs-store-alt"
+    }, {
+        id: 3,
+        title: 'Yangi buyurtma',
+        link: "/order",
+        icon: "bx bxs-cart-add"
+    }, {
+        id: 6,
+        title: 'Kontaktlar',
+        link: "/contact",
+        icon: "bx bxs-contact"
+    }, {
+        id: 7,
+        title: 'Viloyatlar',
+        link: "/region",
+        icon: "bx bxs-map"
+    }, {
+        id: 5,
+        title: 'Sozlanmalar',
+        link: "/settings",
+        icon: "bx bx-cog"
+    },
+
+];
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <div class="p-3">
+        <h2 class="uppercase dark:text-white text-gray-900">Login</h2>
+    </div>
+</template>
+
+
+<style scoped lang="scss">
+
+</style>
